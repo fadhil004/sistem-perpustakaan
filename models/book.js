3edf4c6344f594d73dec0df3b06a692c67413d35
fadhil004 <m.fadhil238@gmail.com>
2024-04-23 22:22:23 +0700

    checkpoint 2

--- models/book.js
+++ models/book.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Book.belongsTo(models.Author)
+      Book.belongsToMany(models.Member, {
+        through: 'Loan'
+      })
     }
   }
   Book.init({
--- models/member.js
+++ models/member.js
@@ -10,7 +10,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Member.belongsToMany(models.Book, {
+        through: 'Loan'
+      })
     }
   }
   Member.init({
